24cb7e215d4aa8d696e1d03433b25e52e7f6bcbb
raulgp <raul.garrigos@hotmail.com>
2023-11-07 15:56:36 +0100

    Iteration 3 - Adding new celebrities - Done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,30 @@
 const router = require("express").Router();
 
-// all your routes here
+const Celebrity = require("../models/Celebrity.model");
+
+// GET "/celebrities/create" => Renderizar un formulario para crear las celebrities
+
+router.get("/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity.hbs");
+});
+
+// POST "/celebrities/create" => Crear la celebrity en la BD con el formulario y redireccionar
+
+router.post("/create", async (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+
+  try {
+    await Celebrity.create({
+      name,
+      occupation,
+      catchPhrase,
+    });
+    res.redirect("/celebrities");
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET "/celebrities" => Renderizar la lista de celebridades creadas
 
 module.exports = router;
